3621d2d1c19d912e0f45c46e606ac104634c6e9f
masum-2025 <amirulislamme@gmail.com>
2025-05-30 21:57:58 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DSE Trading App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DSE Trading App'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to DSE Trading App!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
